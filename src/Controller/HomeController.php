c02e90e7e1abee6824014559fd79b2cf230ae1a3
Antoine <sirpa.antoine@gmail.com>
2023-02-22 09:56:51 +0100

    create form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
-use App\Repository\CityRepository;
-use App\Repository\CountryRepository;
+use App\Entity\City;
+use App\Entity\Country;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(CountryRepository $countryRepository, CityRepository $cityRepository): Response
+    public function index(): Response
     {
-        dd($cityRepository->findAll(), $countryRepository->findAll());
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class)
+            ->add('country', EntityType::class, [
+                'class' => Country::class,
+                'choice_label' => 'name',
+            ])
+            ->add('city', EntityType::class, [
+                'class' => City::class,
+                'choice_label' => 'name',
+            ])
+            ->add('message', TextareaType::class)
+            ->getForm()
+        ;
 
-        return $this->render('home.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->renderForm('home.html.twig', compact('form'));
     }
 }
