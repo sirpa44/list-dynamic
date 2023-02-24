2be02e63ab4498616d2bc5e5cfd40fe4055c1331
Antoine <sirpa.antoine@gmail.com>
2023-02-24 19:03:29 +0100

    test form events

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,29 +6,30 @@ use App\Entity\City;
 use App\Entity\Country;
 use App\Repository\CityRepository;
 use App\Repository\CountryRepository;
-use DateTime;
+use DateTimeImmutable;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(Request $request): Response
     {
-        $form = $this->createFormBuilder()
-            ->add('name', TextType::class, [
-                'constraints' => new NotBlank(['message' => 'please enter your name.']),
+        $form = $this->createFormBuilder(['availableAt' => new DateTimeImmutable('+5 dsys')])
+            ->add('name', TextType::class)
+            ->add('price', NumberType::class, [
+                // 'constraints' => new NotBlank(['message' => 'please enter your name.']),
             ])
             ->add('country', EntityType::class, [
-                'constraints' => new NotBlank(['message' => 'please choose a country.']),
+                // 'constraints' => new NotBlank(['message' => 'please choose a country.']),
                 'placeholder' => 'Choose a country',
                 'class' => Country::class,
                 'choice_label' => 'name',
@@ -37,9 +38,9 @@ class HomeController extends AbstractController
                 }
             ])
             ->add('city', EntityType::class, [
-                'constraints' => new NotBlank(['message' => 'please choose a city.']),
+                // 'constraints' => new NotBlank(['message' => 'please choose a city.']),
                 'placeholder' => 'Choose a city',
-                'disabled' => true,
+                // 'disabled' => true,
                 'class' => City::class,
                 'choice_label' => 'name',
                 'query_builder' => function(CityRepository $countryRepository) {
@@ -47,19 +48,27 @@ class HomeController extends AbstractController
                 }
             ])
             ->add('message', TextareaType::class, [
-                'constraints' => [
-                    new NotBlank(['message' => 'seems like your issue has been resolved :).']),
-                    new Length(['min' => 5]),
-                ]
+                // 'constraints' => [
+                    // new NotBlank(['message' => 'seems like your issue has been resolved :).']),
+                    // new Length(['min' => 5]),
+                // ]
             ])
             ->add('availableAt', DateTimeType::class)
+            ->addEventListener(FormEvents::PRE_SET_DATA, function () {
+                dump('test');
+            })
+            ->addEventListener(FormEvents::POST_SET_DATA, function () {
+                dd('test1');
+            })
             ->getForm()
         ;
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dd('it work so well');
+            dd($form->get('price')->getData(),
+            $form->get('price')->getNormData(),
+            $form->get('price')->getViewData());
         }
 
         return $this->renderForm('home.html.twig', compact('form'));
